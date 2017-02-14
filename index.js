8696a1f63d3b4bff72ba5ad63fceee1529febf56
Fawaz Bello <info@lanraeictsolutions.com>
2017-02-14 12:15:44 +0100

    Necessary files included. #package.json contains CLI information

---
+++ index.js
@@ -0,0 +1,244 @@
+#!/usr/bin/env node
+'use strict'
+var files = require('./lib/files');
+//=================================//
+var chalk       = require('chalk');
+var clear       = require('clear');
+var CLI         = require('clui');
+var figlet      = require('figlet');
+var inquirer    = require('inquirer');
+var Preferences = require('preferences');
+var Spinner     = CLI.Spinner;
+var GitHubApi   = require('github');
+var _           = require('lodash');
+var git         = require('simple-git')();
+var touch       = require('touch');
+var fs          = require('fs');
+
+clear();
+console.log(
+  chalk.blue(
+    figlet.textSync('Andela SLC CLI', { horizontalLayout: 'full' })
+  )
+);
+if (files.directoryExists('.git')) {
+  console.log(chalk.red('Please BFA,This is already a git repository!'));
+  process.exit();
+}
+
+var github = new GitHubApi({ Version: '.0.0'});
+
+function getGithubCredentials(callback) {
+  var questions = [
+    {
+      name: 'username',
+      type: 'input',
+      message: 'Enter your Github username or e-mail address:',
+      validate: function( value ) {
+        if (value.length) {
+          return true;
+        } else {
+          return 'Shebi you have email address, enter it now';
+        }
+      }
+    },
+    {
+      name: 'password',
+      type: 'password',
+      message: 'Enter your password:',
+      validate: function(value) {
+        if (value.length) {
+          return true;
+        } else {
+          return 'How va, bros abeg just enter the password na';
+        }
+      }
+    }
+  ];
+
+  inquirer.prompt(questions).then(callback);
+}
+
+
+function getGithubToken(callback) {
+  var prefs = new Preferences('Andela SLC CLI');
+
+  if (prefs.github &amp;&amp; prefs.github.token) {
+    return callback(null, prefs.github.token);
+  }
+
+  getGithubCredentials(function(credentials) {
+  var status = new Spinner('Authenticating you (You be robot? No?),Okay, please wait...');
+  status.start();
+
+  github.authenticate(
+    _.extend(
+      {
+        type: 'basic',
+      },
+      credentials
+    )
+  );
+
+  github.authorization.create({
+    scopes: ['user', 'public_repo', 'repo', 'repo:status'],
+    note: 'Andela SLC CLI, the command-line tool for initalizing Git repository'
+  }, function(err, res) {
+    status.stop();
+    if ( err ) {
+      return callback( err );
+    }
+    if (res.token) {
+      prefs.github = {
+        token : res.token
+      };
+      return callback(null, res.token);
+    }
+    return callback();
+  });
+});
+}
+
+function createRepo(callback) {
+  var argv = require('minimist')(process.argv.slice(2));
+
+  var questions = [
+    {
+      type: 'input',
+      name: 'name',
+      message: 'Enter a name for the repository:',
+      default: argv._[0] || files.getCurrentDirectoryBase(),
+      validate: function( value ) {
+        if (value.length) {
+          return true;
+        } else {
+          return 'Please enter a alias for the repository';
+        }
+      }
+    },
+    {
+      type: 'input',
+      name: 'description',
+      default: argv._[1] || null,
+      message: 'Optionally enter a description of the repository:'
+    },
+    {
+      type: 'list',
+      name: 'visibility',
+      message: 'Public or private:',
+      choices: [ 'public', 'private' ],
+      default: 'public'
+    }
+  ];
+
+  inquirer.prompt(questions).then(function(answers) {
+    var status = new Spinner('I dey Create the repository, so wait a little...');
+    status.start();
+
+    var data = {
+      name : answers.name,
+      description : answers.description,
+      private : (answers.visibility === 'private')
+    };
+
+    github.repos.create(
+      data,
+      function(err, res) {
+        status.stop();
+        if (err) {
+          return callback(err);
+        }
+        return callback(null, res.ssh_url);
+      }
+    );
+  });
+}
+
+function createGitignore(callback) {
+  var filelist = _.without(fs.readdirSync('.'), '.git', '.gitignore');
+
+  if (filelist.length) {
+    inquirer.prompt(
+      [
+        {
+          type: 'checkbox',
+          name: 'ignore',
+          message: 'Select the files and/or folders you wish to ignore:',
+          choices: filelist,
+          default: ['node_modules', 'bower_components']
+        }
+      ]
+    ).then(function( answers ) {
+        if (answers.ignore.length) {
+          fs.writeFileSync( '.gitignore', answers.ignore.join( '\n' ) );
+        } else {
+          touch( '.gitignore' );
+        }
+        return callback();
+      }
+    );
+  } else {
+    touch('.gitignore');
+    return callback();
+  }
+}
+
+function setupRepo(url, callback) {
+  var status = new Spinner('I dey Set up the repository wey you just create...');
+  status.start();
+
+  git
+    .init()
+    .add('.gitignore')
+    .add('./*')
+    .commit('Initial commit')
+    .addRemote('origin', url)
+    .push('origin', 'master')
+    .then(function(){
+      status.stop();
+      return callback();
+    });
+}
+
+function githubAuth(callback) {
+  getGithubToken(function(err, token) {
+    if (err) {
+      return callback(err);
+    }
+    github.authenticate({
+      type : 'oauth',
+      token : token
+    });
+    return callback(null, token);
+  });
+}
+
+githubAuth(function(err, authed) {
+  if (err) {
+    switch (err.code) {
+      case 401:
+        console.log(chalk.red('Couldn\'t log you in.Abeg use the right information & Please try again.'));
+        break;
+      case 422:
+        console.log(chalk.red('You already have an access token.'));
+        break;
+    }
+  }
+  if (authed) {
+    console.log(chalk.green('Sucessfully authenticated!'));
+    createRepo(function(err, url){
+      if (err) {
+        console.log('An error has occured');
+      }
+      if (url) {
+        createGitignore(function() {
+          setupRepo(url, function(err) {
+            if (!err) {
+              console.log(chalk.green('Finito aka All done! Take care BFA'));
+            }
+          });
+        });
+      }
+    });
+  }
+});
